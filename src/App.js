674bcc517406d50841819f83f2a20bfacd95e3a3
takecaretakecaretakecare <takecaretakecaretakecare@github.com>
2017-10-10 01:06:52 +0300

    Work with link

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 
 var myNews = [
   
-    {
+    {   
         author: 'Саша Печкин',
-        text: 'В  четверг,  четвертого  числа...'
+        text: 'В  четверг,  четвертого  числа...',
+        bigText:  'в  четыре  с четвертью часа  четыре  чёрненьких  чумазеньких чертёнка  чертили чёрными чернилами чертёж.'
+
     },
     {
         author: 'Просто Вася',
-        text: 'Считаю,  что $ должен  стоить  35  рублей!'
+        text: 'Считаю,  что $ должен  стоить  35  рублей!',
+        bigText:  'А  евро  42!'
     },
     {
         author: 'Гость',
-        text: 'Бесплатно. Скачать.  Лучший  сайт  - http://localhost:3000'
+        text: 'Бесплатно. Скачать.  Лучший  сайт  - http://localhost:3000',
+        bigText:  'На самом деле  платно, просто  нужно прочитать очень длинное лицензионное  соглашение'
     }
 
 ];
@@ -21,6 +25,11 @@ var myNews = [
 
 
 class News extends Component { 
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  }
+
+
   render() {
       var data = this.props.data;
       var newsTemplate;
@@ -51,17 +60,29 @@ class News extends Component {
 
 
 class Arcticle extends Component {
-    render() {
+          state = { visible: false };
+
+        readmoreClick = (e) => {
+        e.preventDefault();
+        this.setState({visible: true});
+    };
+
+
+      render() {
         var author = this.props.data.author,
-            text = this.props.data.text;
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
 
 
-        return(
-            <div  className="article">
-                <p  className="news__author">{author}:</p>
-                <p  className="news__text">{text}</p>
-            </div>
-            );
+            return(
+              <div  className="article">
+                  <p  className="news__author">{author}:</p>
+                  <p  className="news__text">{text}</p>
+                  <a href="#" onClick={this.readmoreClick} className= {"news__readMore " + (visible ? 'none':'')}>More</a>
+                  <p className= {"news__bigText " + (visible ?'':'none')}>{bigText}</p>
+              </div>
+              );
       }
 }
 
@@ -75,8 +96,7 @@ class App extends Component {
     return (
       <div className = "app">
           <h3>News</h3>
-          <News data = {myNews} />
-        
+          <News data = {myNews} />       
       </div>
     );
   }
