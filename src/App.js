86a537b3d0f8e71d793475e02306076bb9dd7e34
DYalo <Yalovchik.dima@gmail.com>
2017-10-10 16:33:02 +0300

    Carcass of application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom'
 
-var myNews = [
+var my_news = [
   
     {   
         author: 'Саша Печкин',
@@ -22,13 +23,11 @@ var myNews = [
 
 ];
 
+var {EventEmitter} = require('fbemitter');
 
+window.ee = new EventEmitter();
 
 class News extends Component { 
-  propTypes: {
-    data: React.PropTypes.array.isRequired
-  }
-
 
   render() {
       var data = this.props.data;
@@ -49,7 +48,10 @@ class News extends Component {
     return(
       <div className="news">
           {newsTemplate} 
-          <strong className = { 'news_count ' + (data.length > 0 ? ' ':'none') }> Всего новостей: {data.length}</strong>  
+        <strong
+          className = { 'news_count ' + (data.length > 0 ? ' ':'none') }>
+          Total News: {data.length}
+        </strong>  
       </div>
       );
     }
@@ -57,10 +59,9 @@ class News extends Component {
 }
 
 
-
-
 class Arcticle extends Component {
-          state = { visible: false };
+
+        state = { visible: false };
 
         readmoreClick = (e) => {
         e.preventDefault();
@@ -74,12 +75,17 @@ class Arcticle extends Component {
             bigText = this.props.data.bigText,
             visible = this.state.visible;
 
-
             return(
               <div  className="article">
                   <p  className="news__author">{author}:</p>
                   <p  className="news__text">{text}</p>
-                  <a href="#" onClick={this.readmoreClick} className= {"news__readMore " + (visible ? 'none':'')}>More</a>
+
+                  <a href="#"
+                   onClick={this.readmoreClick}
+                   className= {"news__readMore " + (visible ? 'none':'')}>
+                   More
+                   </a>
+
                   <p className= {"news__bigText " + (visible ?'':'none')}>{bigText}</p>
               </div>
               );
@@ -87,16 +93,113 @@ class Arcticle extends Component {
 }
 
 
+class Add extends Component {
+
+    state = {
+        agreeNotChecked:  true,
+        authorIsEmpty:  true,
+        textIsEmpty:  true
+    }
+
+        componentDidMount()  {
+        ReactDOM.findDOMNode(this.refs.author).focus();
+    }
+
+    onBtnClickHandler = (e) => {
+          e.preventDefault();
+          var textEl  = ReactDOM.findDOMNode(this.refs.text);
+          var author  = ReactDOM.findDOMNode(this.refs.author).value;
+          var text  = textEl.value;
 
 
+          var item = [{
+            author: author,
+            text: text,
+            bigText: '...'
+
+          }];
+          window.ee.emit('News.add', item);
+
+          textEl.value  = '';
+          this.setState({textIsEmpty: true});
+
+
+          //ReactDOM.findDOMNode(this.refs.text).value = '';
+          //ReactDOM.findDOMNode(this.refs.author).value = '';
+    }
+    onCheckRuleClick = (e) => {
+        this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+    }
+
+    onFieldChange = (fieldName, e) => {
+        if(e.target.value.trim().length > 0) {
+            this.setState({['' + fieldName]: false})
+        } else {
+            this.setState({['' + fieldName]: true})
+        }
+    }
+
+    render() {
+      var agreeNotChecked = this.state.agreeNotChecked,
+          authorIsEmpty = this.state.authorIsEmpty,
+          textIsEmpty = this.state.textIsEmpty;
+        return(
+          <form className="add cf">
+          <input 
+            type='text'
+            className="add__author"
+            onChange = {this.onFieldChange.bind(this, 'authorIsEmpty')}
+            defaultValue=''
+            placeholder="your name"
+            ref='author'
+          />
+          <textarea
+            className="add__text"
+            onChange = {this.onFieldChange.bind(this, 'textIsEmpty')}
+            defaultValue=''
+            placeholder='news text'
+            ref='text'
+          ></textarea>
+          <label className='add__checkrule'>
+            <input type='checkbox' ref='checkrule' onChange = {this.onCheckRuleClick}/>I confirm the rules
+          </label>
+          <button
+            className='add__btn'
+            onClick={this.onBtnClickHandler}
+            ref='alert_button'
+            disabled ={agreeNotChecked  ||  authorIsEmpty ||  textIsEmpty}
+            >
+            Add News
+          </button>
+          </form>
+      );
+    }
+}
 
 
 class App extends Component {
+    state = {
+        news: my_news
+    }
+
+    componentDidMount=()=> {
+      var self = this;
+      window.ee.addListener('News.add', function(item) {
+        var nextNews = item.concat(self.state.news);
+        self.setState({news: nextNews});
+      });
+
+    }
+    componentWillUnmount=()=> {
+        window.ee.removeListener('News.add');
+    }
   render() {
     return (
       <div className = "app">
+      <Add/>
           <h3>News</h3>
-          <News data = {myNews} />       
+          
+          <News data = {this.state.news} />       
       </div>
     );
   }
