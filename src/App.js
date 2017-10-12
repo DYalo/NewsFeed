8296bdf76416ff5016208377e938be4d7c2b8176
DYalo <Yalovchik.dima@gmail.com>
2017-10-12 12:27:40 +0300

    Add likes button

--- src/App.js
+++ src/App.js
@@ -1,71 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom'
+import my_news from './resourses.js'
 
-var my_news = [
-  
-    {   
-        author: 'Саша Печкин',
-        text: 'В  четверг,  четвертого  числа...',
-        bigText:  'в  четыре  с четвертью часа  четыре  чёрненьких  чумазеньких чертёнка  чертили чёрными чернилами чертёж.'
-
-    },
-    {
-        author: 'Просто Вася',
-        text: 'Считаю,  что $ должен  стоить  35  рублей!',
-        bigText:  'А  евро  42!'
-    },
-    {
-        author: 'Гость',
-        text: 'Бесплатно. Скачать.  Лучший  сайт  - http://localhost:3000',
-        bigText:  'На самом деле  платно, просто  нужно прочитать очень длинное лицензионное  соглашение'
-    }
-
-];
 
 var {EventEmitter} = require('fbemitter');
 
 window.ee = new EventEmitter();
 
-class News extends Component { 
+class News extends React.PureComponent {
+    render () {
+      const { data } = this.props
+      const { length } = data
 
-  render() {
-      var data = this.props.data;
-      var newsTemplate;
-
-        if(data.length > 0) {
-
-          newsTemplate = data.map(function(item, index) {
-              return (
-                  <div key={index}>
-                    <Arcticle data = {item} />
-                  </div>
-                 );
-              });
-        } else { 
-          newsTemplate = <p>There are no news</p>        
-        }
-    return(
-      <div className="news">
-          {newsTemplate} 
-        <strong
-          className = { 'news_count ' + (data.length > 0 ? ' ':'none') }>
-          Total News: {data.length}
-        </strong>  
-      </div>
-      );
+      return(
+        <div className='news'>
+          { length && data.map((item, index) => (
+            <div key={ index }>
+            <Arcticle data={ item } />
+            </div>
+              ))
+            ||
+            <p>There are no news</p> 
+          }
+          <strong
+            className = { `news_count ${ !length && 'none' }` }>
+            Total News: { length }
+          </strong>
+        </div>
+      )
     }
-
 }
-
 
 class Arcticle extends Component {
 
-        state = { visible: false };
-
-        readmoreClick = (e) => {
-        e.preventDefault();
-        this.setState({visible: true});
+  state = { visible: false };
+  readmoreClick = (e) => {
+  e.preventDefault();
+  this.setState({visible: true});
     };
 
 
@@ -87,11 +59,11 @@ class Arcticle extends Component {
                    </a>
 
                   <p className= {"news__bigText " + (visible ?'':'none')}>{bigText}</p>
+                  <Likes />
               </div>
               );
       }
 }
-
 
 class Add extends Component {
 
@@ -140,9 +112,9 @@ class Add extends Component {
     }
 
     render() {
-      var agreeNotChecked = this.state.agreeNotChecked,
-          authorIsEmpty = this.state.authorIsEmpty,
-          textIsEmpty = this.state.textIsEmpty;
+      var agreeNotChecked, authorIsEmpty, textIsEmpty;
+      [agreeNotChecked , authorIsEmpty ,textIsEmpty ,] = [this.state.agreeNotChecked,this.state.authorIsEmpty ,this.state.textIsEmpty];
+        
         return(
           <form className="add cf">
           <input 
@@ -176,24 +148,55 @@ class Add extends Component {
     }
 }
 
+class Likes extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {like: 0};
+
+      this.onIncrease = this.onIncrease.bind(this);
+      this.onDecrease = this.onDecrease.bind(this);
+  }
+
+  onIncrease() {
+      this.setState(prevState => ({
+        like: prevState.like + 1
+      }))
+  }
+  onDecrease() {
+      this.setState(prevState => ({
+        like: prevState.like - 1
+      }))
+  }
+
+  render() {
+      return(
+        <button onClick={this.onIncrease}>like</button>
+        <p>Likes:{this.state.like}</p>
+        <button onClick={this.onDecrease}>like</button>
+        );
+  }
+
+}
+
 
 class App extends Component {
-    state = {
-        news: my_news
-    }
+  state = {
+    news: my_news
+   }
 
-    componentDidMount=()=> {
-      var self = this;
-      window.ee.addListener('News.add', function(item) {
-        var nextNews = item.concat(self.state.news);
-        self.setState({news: nextNews});
-      });
+  componentDidMount=()=> {
+   var self = this;
+   window.ee.addListener('News.add', function(item) {
+   var nextNews = item.concat(self.state.news);
+   self.setState({news: nextNews});
+    });
 
     }
     componentWillUnmount=()=> {
         window.ee.removeListener('News.add');
     }
-  render() {
+ render() {
     return (
       <div className = "app">
       <Add/>
